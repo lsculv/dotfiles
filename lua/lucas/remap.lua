95908cb2f740c93a14fa2dae35e94fafbfe24d50
lsculv <112150142+lsculv@users.noreply.github.com>
2023-07-02 23:16:20 -0500

    Add keymap for auto formatting the current file/project

--- lua/lucas/remap.lua
+++ lua/lucas/remap.lua
@@ -4,3 +4,26 @@ vim.keymap.set("n", "<C-k>", vim.lsp.buf.hover)
 vim.keymap.set("n", "<F9>", ":!%:p<CR>")
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader><leader>", "<C-^>")
+
+-- Formatting current file/project
+local filetypes = {
+    rust = ":!cargo fmt",
+    ocaml = ":!dune fmt",
+    go = ":!go fmt",
+    python = ":!black %"
+}
+
+function Format()
+    local filetype = vim.bo.filetype
+    local command = filetypes[filetype]
+    if command then
+        vim.cmd(command)
+    else
+        print("Error: No formatting command defined for this file type.")
+    end
+
+    return cmd
+end
+
+vim.api.nvim_set_keymap("n", "<leader>fm", "<Cmd>lua Format()<Cr>", { silent = false } )
+
