e273a67b36a67b09796f7667abcf65d005f182d4
Lucas Culverhouse <lucasculverhouse@gmail.com>
2023-11-17 15:48:23 -0800

    Lazy: switch from Packer to lazy.nvim

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,11 +1,56 @@
 local lsp = require("lsp-zero")
 
-lsp.preset("recommended")
+local lua_opts = lsp.nvim_lua_ls()
+require('lspconfig').lua_ls.setup(lua_opts)
 
-lsp.ensure_installed({
-	"rust_analyzer",
-	"pyright"
+require("mason").setup()
+require('mason-lspconfig').setup({
+    handlers = {
+        lsp.default_setup,
+    }
 })
+
+local cmp = require('cmp')
+local cmp_action = lsp.cmp_action()
+require('luasnip.loaders.from_vscode').lazy_load()
+
+cmp.setup({
+    preselect = 'item',
+    completion = {
+        completeopt = 'menu,menuone,noinsert',
+    },
+    window = {
+    documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        -- confirm completion item
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+        -- toggle completion menu
+        ['<C-e>'] = cmp_action.toggle_completion(),
+
+        -- tab complete
+        ['<Tab>'] = cmp_action.tab_complete(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+        -- navigate between snippet placeholder
+        ['<C-d>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+        -- scroll documentation window
+        ['<C-f>'] = cmp.mapping.scroll_docs(-5),
+        ['<C-u>'] = cmp.mapping.scroll_docs(5),
+    }),
+    sources = {
+        {name = 'path'},
+        {name = 'nvim_lsp'},
+        {name = 'nvim_lua'},
+        {name = 'buffer', keyword_length = 3},
+        {name = 'luasnip', keyword_length = 2},
+  },
+})
+
+lsp.preset("recommended")
 
 lsp.on_attach(function(client, buff)
     local opts = { buffer = buff, remap = false}
--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,18 +1,12 @@
---vim.o.background = "dark"
+-- vim.o.background = "dark"
 
-require("github-theme").setup({
-    keyword_style = "bold",
-    comment_style = "none",
+vim.g.lightline = { colorscheme = 'rosepine' }
 
+require('rose-pine').setup({
+    disable_italics = true,
 })
 
-vim.g.lightline = { colorscheme = "deus" }
+vim.cmd('colorscheme rose-pine')
 
 vim.cmd('autocmd FileType markdown setlocal spell')
 vim.cmd('autocmd FileType gitcommit setlocal spell')
-
-
---require("gruvbox").setup({
-	--italic = false,
---})
---vim.cmd([[colorscheme gruvbox]])
