0f4139d67ef6dbdcf6139ddf87d297da038365ed
Lucas Culverhouse <lucasculverhouse@gmail.com>
2024-02-06 12:58:25 -0800

    Fix rose-pine colorscheme

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,12 +1,12 @@
--- vim.o.background = "dark"
+vim.o.background = "dark"
 
-vim.g.lightline = { colorscheme = 'rosepine' }
+vim.g.lightline = { colorscheme = 'rosepine_moon' }
 
 require('rose-pine').setup({
     disable_italics = true,
 })
 
-vim.cmd('colorscheme rose-pine')
+vim.cmd('colorscheme rose-pine-moon')
 
 vim.cmd('autocmd FileType markdown setlocal spell')
 vim.cmd('autocmd FileType gitcommit setlocal spell')
